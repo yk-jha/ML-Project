c1d2ea7aec8b8dd8451f3f5747af4725c9533e26
yk-jha <yjha5965@gmail.com>
2025-02-04 13:57:58 +0530

    logging success

--- src/logger.py
+++ src/logger.py
@@ -6,7 +6,7 @@ LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
 logs_path = os.path.join(os.getcwd() , "logs" , LOG_FILE)
 
-os.makdirs(logs_path , exist_ok = True)
+os.makedirs(logs_path , exist_ok = True)
 
 LOG_FILE_PATH = os.path.join(logs_path , LOG_FILE)
 
